876f385eca649a7c44fc1d11753ed7dc8e878c21
Tyrah <tyrah96@gmail.com>
2022-10-30 16:22:35 -0700

    color and size sections on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,10 +17,13 @@ const Home: FC<HomeProps> = ({title}) => {
                     <h1>{title}</h1>
                 </div> 
                 <div className='home-flex-3'>
-                    <p className='home-text-1'>Tyrah D. Gullette</p>
+                    <p className='home-text-1'>Tyrah D. Gullette, Software Engineering Intern @ Salesforce</p>
+                </div>
+                <div className='home-flex-3add'>
+                    <p className='home-text-1add'>Hello,</p>
                 </div>
                 <div className='home-flex-4'>
-                    <p className='home-text-2'>Hello, I’m Tyrah, a software engineer. I build performant and scalable software using forward-thinking methods.</p>
+                    <p className='home-text-2'>I’m Tyrah, a software engineer. I build performant and scalable software using forward-thinking methods.</p>
                 </div>
                 <div className='home-flex-5'>
                     <p className='home-text-3'>Adventurous Tech developer &
@@ -30,17 +33,26 @@ Raised in the Y2K era of tech optimism.</p>
             </div>
             <section className='section-1'>
                 <div>
-                    <p className='home-text-4'> hi there</p>
+                    <p className='home-text-4'> Tell Me where</p>
+                    <p className='project-1-text-1'>Description</p>
+                    <p className='project-1-text-2'>Image</p>
+                    <p className='project-1-text-3'>Link</p>
                 </div>
             </section>
             <section className='section-2'>
                 <div>
-                    <p className='home-text-5'> hi there 2 </p>
+                    <p className='home-text-5'> Viewing Party </p>
+                    <p>Description</p>
+                    <p>Image</p>
+                    <p>Link</p>
                 </div>
             </section>
             <section className='section-3'>
                 <div>
-                    <p className='home-text-6'> hi there 3 </p>
+                    <p className='home-text-6'> Inspiration Board </p>
+                    <p>Description</p>
+                    <p>Image</p>
+                    <p>Link</p>
                 </div>
             </section>
         </div>
