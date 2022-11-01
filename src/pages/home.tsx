312e0c547350abbd861a5a4e13239d49c1a80650
Tyrah <tyrah96@gmail.com>
2022-10-31 18:51:05 -0700

    pics added, viewing party and tell me where uploaded

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,7 +17,7 @@ const Home: FC<HomeProps> = ({title}) => {
                     <h1>{title}</h1>
                 </div> 
                 <div className='home-flex-4add'>
-                    <img src={require('../images/uranus.png')} alt='uranus' width={280} height={300}/> 
+                    <img src={require('../images/saturn.png')} alt='uranus' width={280} height={300}/> 
                 </div>
                 <div className='home-flex-3'>
                     <p className='home-text-1'>Tyrah D. Gullette, Software Engineering Intern @ Salesforce</p>
@@ -41,16 +41,40 @@ Raised in the Y2K era of tech optimism.</p>
                 <section className='section-1'>
                     <div>
                         <p className='home-text-4'> Tell Me where</p>
-                        <p className='project-1-text-1'>Description</p>
-                        <p className='project-1-text-2'>Image</p>
-                        <a className='project-1-text-3' target='_blank' rel='noreferrer' href='https://github.com/ursaturnine/tell-me-where-app'>View in Github</a>
+                        <p className='project-1-text-1'>A mobile app for sharing restaurant recommendations with friends. Tell Me Where gives you recommendations you can rely on because, unlike other food place recommendation apps, Tell Me Where offers reviews from the people you trust most -your friends and family.The backend for Tell Me
+                        Where was coded with Flask and the frontend uses the React Native framework. </p>
+                        <p className='project-1-text-6'>Features</p>
+                        <p className='project-1-text-9'>Add a User, Add a Friend, Delete a Friend, Add a Recommendation, Delete a Recommendation, Search Recs by Location, View Recs By User</p>
+                        <p className='project-1-text-4'>The backend for Tell Me Where employs RESTful routes for applicable CRUD operations. It includes a User model and a Recommendation model for a many-to-many PSQL database. The custom helper functions anticpate the data needed from the Yelp API and sanitizes it to be used in Tell Me Where's frontend logic. </p>
+                        <p className='project-1-text-5'>The frontend for Tell Me Where intelligently uses React primitives to create reusable componenents and hooks for each page. The file structure is organized for scalability making room for additional API's, hooks, contexts, screens and components to be systematically added.</p>
+                        <p className='project-1-text-7'>Technologies</p>
+                        <p className='project-1-text-8'>React Native, Python, Yelp API, Flask, SQLAlchemy, PSQL, Expo Go</p>
+                        <img className='project-1-text-2' src={require('../images/search_by_city.jpg')} alt='figma prototype' width={230} height={230}/>
+                        <i className='project-1-text-11'>Figma Design of Searching a Rec By a City</i>
+                        <a className='project-1-text-3' target='_blank' rel='noreferrer' href='https://github.com/ursaturnine/tell-me-where-app'>View Frontend Code in Github</a>
+                        <a className='project-1-text-10' target='_blank' rel='noreferrer' href='https://github.com/lili4x4/tell-me-where-backend'>View Backend Code in Github</a>
                     </div>
                 </section>
                 <section className='section-2'>
                     <div>
                         <p className='home-text-5'> Viewing Party </p>
-                        <p className='project-2-text-1'>Description</p>
-                        <p className='project-2-text-2'>Image</p>
+                        <p className='project-2-text-1'>A mini project to create a recommendation based algorithm written in the Python language. This project exhibits my ease with using the Python language after 3 years of coding in it.
+                        This project also demonstrates my ability to read a description for a feature and to write a function for it; this coding challenge required I pass the given tests with my logic. In Viewing Party, I use the Python data structures, list and dictionary, to create a movie dictionary for individual users and their friends. </p>
+                        <img className='project-2-text-2' src={require('../images/watch-party.png')} alt='pizza party' height={300} width={300}/>
+                        <p className='project-2-text-4'>Functions: Here are some functions I wrote given tests and descriptions of features. I built off of this logic to code the Tell Me Where app. </p>
+                        <ul className='project-2-text-6'>
+                            <li>Create a movie</li>
+                            <li>Add movie to watched list in nested user dictionary</li>
+                            <li>Add movie to watchlist in nested user dictionary</li>
+                            <li>Get average rating of watched movies in nested user dictionary</li>
+                            <li>Get most watched genre in nested user dictionary</li>
+                            <li>Get unique watched movies in nested user dictionary</li>
+                            <li>Get unique wathced movies from friends nested user dictionaries</li>
+                            <li>Get available recommendations from friends nested user dictionaries</li>
+                            <li>Get new recommendation from friends' nested user dictionaries</li>
+                            <li>Get new recommendation from friends' nest user dictionaries favorites list</li>
+                        </ul>
+                        <p className='project-2-text-5'>Technologies: Python, PyTest, Git</p>
                         <a className='project-2-text-3' target='_blank' href='https://github.com/ursaturnine/viewing-party' rel='noreferrer'>View in Github</a>
                     </div>
                 </section>
@@ -59,7 +83,17 @@ Raised in the Y2K era of tech optimism.</p>
                         <p className='home-text-6'> Inspiration Board </p>
                         <p className='project-3-text-1'>Description</p>
                         <p className='project-3-text-2'>Image</p>
-                        <a className='project-3-text-3' target='_blank' href='https://github.com/ursaturnine/inspo-board-front' rel='noreferrer'>View in Github</a>
+                        <a className='project-3-text-3' target='_blank' href='https://orange-purple.herokuapp.com/' rel='noreferrer'>orange-purple.herokuapp.com</a>
+                        <a className='project-3-text-3' target='_blank' href='https://github.com/ursaturnine/inspo-board-front' rel='noreferrer'>View Frontend Code in Github</a>
+                        <a className='project-3-text-3' target='_blank' href='https://github.com/cathos/back-end-inspiration-board' rel='noreferrer'>View Backend Code in Github</a>
+                    </div>
+                </section>
+                <section className='section-4'>
+                    <div>
+                        <p className='home-text-7'> Weather Report </p>
+                        <p className='project-4-text-1'>Description</p>
+                        <p className='project-4-text-2'>Image</p>
+                        <a className='project-4-text-3' target='_blank' href='https://github.com/ursaturnine/weather-report' rel='noreferrer'>View in Github</a>
                     </div>
                 </section>
             </div>
