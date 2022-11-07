6071d1f88d54aa93dd05c13b67652b18ed0c9c70
Tyrah <tyrah96@gmail.com>
2022-11-06 17:20:05 -0800

    about page text and boxes margins and appearance

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -35,9 +35,10 @@ const About: FC<AboutProps> = ({title}) => {
             <div className='about-sections'>
                 <section className='about-section-1'>
                     <div>
-                        <p className='about-section-1-text-1'>I'm a software developer of four years and deferred cybersecurity major with a proclivity toward backend and cloud technologies. I'm recognized
-                        as a highly curious, quick studied and researched engineer. I'm experienced in working in interoperable teams in fast-paced environments within budget constraints on enterprise-level software. I place heavy emphasis
-                        on custom, scalable logic and iterative development. </p>
+                        <p className='about-section-1-text-1'>I'm a deferred cybersecurity major attending the Ada Developer's Academy for women and gender expansive
+                        folx. I'll be graduating in cohort 17 in January 2023. As a first gen student and black woman in tech, nothing feels like a given and taking a year off from
+                        a degree is a slippery slope. As an applied scientist and highly curious technologist, exploring my chosen field in the way which excites me most, through hands on experience,
+                        is worth the chanciness. I'm hoping my non-linear path will scale to O(logn) instead O(n logn). 🤓 </p>
                         <p className='about-section-1-text-2'>I'm currently a software engineering intern at Salesforce Tableau on the Visibility and Interactivity team.
                         I'm writing code on a project developed to provide an adaptive and creative way to render new Tableau visualizations to satisfy and delight our customers.</p>
                         <p className='about-section-1-text-3'>I love exploring new technologies and starting new projects in my free-time; check out my Works In Progress page to view what I'm designing now. I'm also a huge reader, alternative R&B listener, cook, and anime nerd. </p>
@@ -50,8 +51,8 @@ const About: FC<AboutProps> = ({title}) => {
                 </section>
                 <section className='about-section-3'>
                     <div>
-                        <p>"In the end, the shape and form  don't matter at all. It's only the soul that matters, right? Nothing else."</p>
-                        <i>-Soul Eater Evans</i>
+                        <p className='about-section-3-text-1' >"In the end, the shape and form  don't matter at all. It's only the soul that matters, right? Nothing else."</p>
+                        <i className='about-section-3-text-2'>-Soul Eater Evans</i>
                     </div>
                 </section>
             </div>
