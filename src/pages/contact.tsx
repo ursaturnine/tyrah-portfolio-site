94bd103db253dfa8c58800e9233810f1ad33943e
ursaturnine <tyrah96@gmail.com>
2025-10-19 12:09:06 -0700

    update contact info

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -25,13 +25,7 @@ const Contact: FC<ContactProps> = ({title}) => {
                     <a className='contact-section-2' target='_blank' rel='noreferrer' href='https://github.com/ursaturnine'>
                         <img src={require('../images/github.png')} alt='' width={250} height={250}/>
                     </a>
-                    <a className='contact-section-3'  target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/tyrahgullette/'>
-                        <img src={require('../images/linkedin.png')} alt='' width={250} height={250}/>
-                    </a>
-                    <a className='contact-section-4'  target='_blank' rel='noreferrer' href='https://twitter.com/cloudtyrah'>
-                        <img src={require('../images/twitter.png')} alt='' width={250} height={250}/>
-                    </a>
-                    <a className='contact-section-5'  target='_blank' rel='noreferrer' href='mailto:tyrah96@gmail.com'>
+                    <a className='contact-section-3'  target='_blank' rel='noreferrer' href='mailto:tye79293@gmail.com'>
                     <img src={require('../images/email.png')} alt='' width={250} height={250}/>
                     </a>
                 </section>
